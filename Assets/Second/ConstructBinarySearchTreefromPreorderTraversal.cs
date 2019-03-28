0bc6650c7e8ab0699affe696f3575c0e055ae87c
Dftty <tyzhangyj@163.com>
2019-03-28 21:09:37 +0800

    finish 1008

--- Assets/Second/ConstructBinarySearchTreefromPreorderTraversal.cs
+++ Assets/Second/ConstructBinarySearchTreefromPreorderTraversal.cs
@@ -11,7 +11,20 @@ public class ConstructBinarySearchTreefromPreorderTraversal : MonoBehaviour {
 
 
     /**
+    Construct Binary Search Tree from Preorder Traversal
 
+    (Recall that a binary search tree is a binary tree where for every node, 
+    any descendant of node.left has a value < node.val, and any descendant of node.
+    right has a value > node.val.  Also recall that a preorder traversal displays the 
+    value of the node first, then traverses node.left, then traverses node.right.)
+
+    解法来自Discuss
+
+    本题目给了一个二叉查找树的前序遍历数组，要求用这个数组构建这个二叉查找树
+    这个类型的题目是经典的递归题，
+    因为给定的条件是前序遍历，因此第一个数一定是根节点，然后后面的数组中小于这个数的子数组一定是它的左子树，大于这个数的子数组一定是它的右子树。
+    这样分割后再创建结点并分割，最终就可以求得这个二叉查找树
+    第一种解法
      */
     public TreeNode BstFromPreorder(int[] preorder) {
         return Generate(preorder, 0, preorder.Length - 1);
@@ -40,6 +53,8 @@ public class ConstructBinarySearchTreefromPreorderTraversal : MonoBehaviour {
 
     /**
     第二种解法
+
+    这个解法类似，找到了递归返回的边界。
      */
     int i = 0;
     public TreeNode BstFromPreorder_(int[] preorder){
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,4 @@
 | Squence | Problem       | Level  | Language  | Tags | Video Tutorial|
 |:-------:|:--------------|:------:|:---------:|:----:|:-------------:|
+|1008|[ConstructBinarySearchTreefromPreorderTraversal](https://github.com/dftty/LeetCode/blob/master/Assets/Second/ConstructBinarySearchTreefromPreorderTraversal.cs)|Medium|C#|[Tree]||
 |1024|[CapacityToShipPackagesWithinDDays.cs](https://github.com/dftty/LeetCode/blob/master/Assets/Second/CapacityToShipPackagesWithinDDays.cs)|Medium|C#|[Array Binary Search]||

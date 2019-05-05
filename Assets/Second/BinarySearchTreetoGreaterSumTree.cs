a498654a09a4a42bd8437e553b7c594e45e342fe
zhangyanjun <tyzhangyj@163.com>
2019-05-05 16:52:42 +0800

    test

---
+++ Assets/Second/BinarySearchTreetoGreaterSumTree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinarySearchTreetoGreaterSumTree : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	int count = 0;
+	/**
+	遍历顺序改成右中左，然后计算val即可
+	 */
+    public TreeNode BstToGst(TreeNode root) {
+        DFS(root);
+        
+        return root;
+    }
+    
+    public void DFS(TreeNode root){
+        if(root == null) return ;
+        
+        DFS(root.right);
+        root.val += count;
+        count = root.val;
+        DFS(root.left);
+    }
+}
---
+++ Assets/Second/ValidBoomerang.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ValidBoomerang : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /**
+    对比两条线的斜率是否相等即可
+     */
+	public bool IsBoomerang(int[][] points) {
+        Array.Sort(points, (a, b) => {
+            return a[0] - b[0];
+        });
+        
+        if(points[0][0] == points[1][0] && points[0][1] == points[1][1]) return false;
+        if(points[1][0] == points[2][0] && points[1][1] == points[2][1]) return false;
+        if(points[0][0] == points[2][0] && points[0][1] == points[2][1]) return false;
+        float first = (float)(points[2][1] - points[1][1]) / (float)(points[2][0] - points[1][0]);
+        float second = (float)(points[1][1] - points[0][1]) / (float)(points[1][0] - points[0][0]);
+        
+        return first != second;
+    }
+}

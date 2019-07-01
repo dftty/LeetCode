0ecf2c9f452fee074ab232c7b23dce27c0808a7f
zhangYJ <tyzhangyj@163.com>
2019-07-01 18:37:10 +0800

    test

--- Assets/GameFrameWork/Resource/LoadSceneSuccessCallback.cs
+++ Assets/GameFrameWork/Resource/LoadSceneSuccessCallback.cs
@@ -1,4 +1,10 @@
 namespace GameFramework.Resources
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sceneAssetName"></param>
+    /// <param name="duration"></param>
+    /// <param name="userData"></param>
     public delegate void LoadSceneSuccessCallback(string sceneAssetName, float duration, object userData);
 }

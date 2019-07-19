8ed376dafbc626f04e065ea890ae311d7f08f899
zhangYJ <tyzhangyj@163.com>
2019-07-19 17:45:05 +0800

    test

--- Assets/Second/InterleavingString.cs
+++ Assets/Second/InterleavingString.cs
@@ -22,6 +22,8 @@ public class InterleavingString : MonoBehaviour
     动态规划dp[i, j] 表示长度为i的s1 和长度为j的s2 是否可以组成长度为i + j 的s3 字符串
     dp[i, 0] 表示长度为i的s1 字符串是否可以按顺讯组成长度为i的s3 字符串
     dp[0, j] 表示长度为j的s2 字符串是否可以按顺序组成长度为j的s3 字符串
+
+    做题的时候在第二个bool值赋值的时候出现了之前赋值为true没有进行或操作的错误
      */
     public bool IsInterleave(string s1, string s2, string s3) {
         int m = s1.Length, n = s2.Length;
@@ -43,13 +45,14 @@ public class InterleavingString : MonoBehaviour
         }
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
-                if(s1[i - 1] == s3[i + j - 1]){
-                    dp[i, j] = dp[i - 1, j];
-                }
+                // if(s1[i - 1] == s3[i + j - 1]){
+                //     dp[i, j] = dp[i - 1, j];
+                // }
                 
-                if(s2[j - 1] == s3[i + j - 1]){
-                    dp[i, j] = dp[i, j - 1] || dp[i, j];
-                }
+                // if(s2[j - 1] == s3[i + j - 1]){
+                //     dp[i, j] = dp[i, j - 1] || dp[i, j];
+                // }
+                dp[i, j] = (dp[i - 1, j] && s1[i - 1] == s3[i + j - 1]) || (dp[i, j - 1] && s2[j - 1] == s3[i + j - 1]);
             }
         }
         return dp[m , n];

5e48d32086108b857c40a138b03265d6b648004e
zhangYJ <tyzhangyj@163.com>
2019-09-17 17:20:46 +0800

    test

---
+++ Assets/Second/PermutationsII.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PermutationsII : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+    https://leetcode.com/problems/permutations-ii/
+
+    技巧，对于需要进行判断是否使用的，如果是独一无二的元素，可以考虑HashSet
+    如果有重复的元素，可以考虑使用bool数组记录每个下标的使用情况
+
+    回溯法, 对于重复的数字，可以先将数组进行排序，然后使用一个bool值数组记录是否使用当前数字
+     */
+    public IList<IList<int>> PermuteUnique(int[] nums) {
+        IList<IList<int>> res = new List<IList<int>>();
+        if (nums == null || nums.Length == 0) return res;
+        Array.Sort(nums);
+        Recursion(res, new List<int>(), nums, new bool[nums.Length]);
+        return res;
+    }
+    
+    public void Recursion(IList<IList<int>> res, IList<int> list, int[] nums, bool[] use){
+        if (list.Count == nums.Length){
+            res.Add(new List<int>(list));
+            return ;
+        }
+        
+        for (int i = 0; i < nums.Length; i++){
+            if (use[i]) continue;
+            if (i > 0 && !use[i] && !use[i - 1] && nums[i] == nums[i - 1]){
+                continue;
+            }
+            
+            list.Add(nums[i]);
+            use[i] = true;
+            Recursion(res, list, nums, use);
+            list.RemoveAt(list.Count - 1);
+            use[i] = false;
+        }
+    }
+}
---
+++ Assets/Second/ReverseSubstringsBetweenEachPairofParentheses.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+
+public class ReverseSubstringsBetweenEachPairofParentheses : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+    https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+
+    递归解法
+     */
+    public string ReverseParentheses(string s) {
+        int position = s.IndexOf('(');
+        
+        if (position == -1) return s;
+        
+        int level = 0;
+        for (int i = position; i < s.Length; i++){
+            if (s[i] == '('){
+                level++;
+            }else if(s[i] == ')'){
+                level--;
+                
+                if (level == 0){
+                    string ans = s.Substring(0, position);
+                    string sub = s.Substring(position + 1, i - position - 1);
+                    sub = ReverseParentheses(sub);
+                    char[] ch = sub.ToCharArray();
+                    Array.Reverse(ch);
+                    sub = new string(ch);
+                    ans = ans + sub + ReverseParentheses(s.Substring(i + 1));
+                    return ans;
+                }
+            }
+        }
+        
+        return "";
+    }
+
+    /**
+    非递归解法
+     */
+    public string ReverseParentheses_(string s) {
+        StringBuilder res = new StringBuilder();
+        Stack<char> stack = new Stack<char>();
+        List<StringBuilder> list = new List<StringBuilder>();
+        list.Add(new StringBuilder(""));
+        foreach (char c in s){
+            if (c == '('){
+                stack.Push(c);
+                list.Add(new StringBuilder(""));
+            }else if (stack.Count > 0 && c == ')'){
+                char[] arr = list[list.Count - 1].ToString().ToCharArray();
+                Array.Reverse(arr);
+                list[list.Count - 2].Append(new string(arr));
+                list.RemoveAt(list.Count - 1);
+                stack.Pop();
+            }else {
+                list[list.Count - 1].Append(c);
+            }
+        }
+        
+        foreach(StringBuilder sbStr in list){
+            res.Append(sbStr);
+        }
+        
+        return res.ToString();
+    }
+}

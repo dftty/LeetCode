7bdb7f8284c3ff0445163793ca64b1f4c341643b
zhangYJ <tyzhangyj@163.com>
2019-10-22 17:04:20 +0800

    test

---
+++ Assets/Second/DP/DiceRollSimulation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRollSimulation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+    https://leetcode.com/problems/dice-roll-simulation/
+
+    
+    https://leetcode.com/problems/dice-roll-simulation/discuss/403756/Java-Share-my-DP-solution
+
+    动态规划题目
+     */
+    public int DieSimulator(int n, int[] rollMax) {
+        long[,] dp = new long[n, 7];
+        
+        for (int i = 0; i < 6; i++){
+            dp[0, i] = 1;
+        }
+        
+        dp[0, 6] = 6;
+        
+        int MOD = 1000000007;
+        
+        for (int i = 1; i < n; i++){
+            long sum = 0;
+            for (int j = 0; j < 6; j++){
+                dp[i, j] = dp[i - 1, 6];
+                
+                if (i - rollMax[j] == 0){
+                    dp[i, j] -= 1;
+                }
+                
+                if (i - rollMax[j] > 0){
+                    long reduce = dp[i - rollMax[j] - 1, 6] - dp[i - rollMax[j] - 1, j];
+                    // 加一个MOD是因为减法有可能出现负数的情况
+                    dp[i, j] = ((dp[i, j] - reduce) % MOD + MOD) % MOD;
+                }
+                
+                sum = (sum + dp[i, j]) % MOD;
+            }
+            
+            dp[i, 6] = sum;
+        }
+        
+        
+        return (int)dp[n - 1, 6];
+    }
+}
---
+++ Assets/Second/FruitIntoBaskets.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FruitIntoBaskets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+    https://leetcode.com/problems/fruit-into-baskets/
+
+    题目表述非常不清晰，看了Discuss后发现题目的意思是
+    找到一个子数组，这个子数组中最多包含两个不同的数字
+
+    滑动窗口算法，使用一个count数组对tree数组中出现的数字进行计数
+    使用itemNum来表示当前子数组中使用了多少不同的数字,遍历过程中，如果当前数字的count为0，那么itemNum加一，
+    如果itemNum大于子数组中要求的最大数字的数量，我们就要从左向右开始删除前面出现的数字，知道其中一个数字的数量为0为止。
+     */
+    public int TotalFruit(int[] tree) {
+        int maxDistinceNum = 2;
+        int itemNum = 0;
+        int[] count = new int[40001];
+        
+        int res = 0;
+        int left = 0;
+        for (int i = 0; i < tree.Length; i++){
+            int n = tree[i];
+            if (count[n] == 0){
+                itemNum++;
+            }
+            count[n]++;
+            while(itemNum > maxDistinceNum && left < i){
+                count[tree[left]]--;
+                if (count[tree[left]] == 0){
+                    itemNum--;
+                }
+                left++;
+            }
+            
+            res = Math.Max(res, i - left + 1);
+        }
+        
+        return res;
+    }
+}

16795da902f93b76e11fd2415c6afc480af67fc2
zhangYJ <tyzhangyj@163.com>
2019-12-04 20:22:41 +0800

    test

---
+++ Assets/Third/Medium/1277.CountSquareSubmatriceswithAllOnes.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Third
+{
+    public class CountSquareSubmatriceswithAllOnes : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        /**
+        https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+        Medium
+        Tag: 数组  动态规划
+
+        思路： 当前格子可以组成正方形的数量和左边，上边和左上格子三个格子中的最小的正方形数量有关。
+        dp[i, j] = min(dp[i - 1, j], dp[i, j - 1], dp[i - 1,j - 1]) + 1
+
+        dp[i, j]的意思是以matrix[i][j] 为右下角组成的最大正方形的长度
+
+        O(mn) time O(mn) space
+        */
+        public int CountSquares(int[][] matrix) {
+            int m = matrix.Length, n = matrix[0].Length;
+            
+            int[,] dp = new int[m, n];
+            int res = 0;
+            for (int i = 0; i < m; i++){
+                for (int j = 0; j < n; j++){
+                    if (matrix[i][j] == 1){
+                        if (i > 0 && j > 0){
+                            dp[i, j] = Math.Min(Math.Min(dp[i - 1, j], dp[i, j - 1]), dp[i - 1, j - 1]) + 1;
+                        }else{
+                            dp[i, j] = 1;
+                        }
+                        
+                        res += dp[i, j];
+                    }
+                }
+            }
+            
+            return res;
+        }
+
+        /**
+        Discuss 解法
+
+        思路：和我的解法类似，没有额外使用数组来保存计算的值。
+        
+        O(mn) time O(1) space
+        */
+        public int CountSquares_Discuss(int[][] matrix) {
+            int m = matrix.Length, n = matrix[0].Length;
+            int res = 0;
+            for (int i = 0; i < m; i++){
+                for (int j = 0; j < n; j++){
+                    if (matrix[i][j] > 0 && i > 0 && j > 0){
+                        matrix[i][j] = Math.Min(Math.Min(matrix[i - 1][j], matrix[i][j- 1]), matrix[i - 1][j - 1]) + 1;
+                    }
+                    res += matrix[i][j];
+                }
+            }
+            
+            return res;
+        }
+
+    }
+}
---
+++ Assets/Third/Medium/34.FindFirstandLastPositionofElementinSortedArray.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Third
+{
+    public class FindFirstandLastPositionofElementinSortedArray : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        /**
+        https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+        Medium
+        Tag: 数组 二分查找
+
+        思路：有序数组中查找一个数，可以立即想到二分查找，题目中要求找到目标数字的
+        最开始出现的位置和最后出现的位置。
+        因此我们可以利用二分查找的特性，分别用两次二分查找，第一次找到最开始出现的位置。
+
+        寻找最开始出现的位置时，如果middle的值和target相等，那么我们就让hi = middle
+        这样最终跳出循环时，lo的位置指向的位置肯定是target的位置(如果target在数组中存在)。
+
+        寻找最后出现的位置时，令 middle = (lo + hi + 1) / 2 这样可以保证middle的位置
+        始终在向右移动。
+
+        */
+        public int[] SearchRange(int[] nums, int target) {
+            int[] res = new int[2]{-1, -1};
+            if (nums == null || nums.Length == 0){
+                return res;
+            }
+            
+            int lo = 0, hi = nums.Length - 1;
+            while(lo < hi){
+                int middle = (lo + hi) / 2;
+                
+                if (nums[middle] < target){
+                    lo = middle + 1;
+                }else {
+                    hi = middle;
+                }
+            }
+            
+            if (nums[lo] != target){
+                return res;
+            }
+            
+            res[0] = lo;
+            lo = 0;
+            hi = nums.Length - 1;
+            
+            while(lo < hi){
+                int middle = (lo + hi + 1) / 2; // 加一保证每次计算都在lo的右边
+                
+                if(nums[middle] > target){
+                    hi = middle - 1;
+                }else{
+                    lo = middle;
+                }
+            }
+            res[1] = hi;
+            return res;
+        }
+    }
+
+}

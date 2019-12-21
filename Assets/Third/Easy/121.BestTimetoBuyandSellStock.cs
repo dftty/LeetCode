406ceb1fc1c42364bcd8c4ead42007eb7d40eca9
zhangYJ <tyzhangyj@163.com>
2019-12-21 18:48:28 +0800

    test

---
+++ Assets/Third/Easy/121.BestTimetoBuyandSellStock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Third
+{
+
+    public class BestTimetoBuyandSellStock : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        /**
+        https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+        Easy
+        Tag: 数组 动态规划
+
+        思路：使用一个min变量记录当前遍历到的数字的前面一个最小值，
+        然后用当前值减去最小值即可
+
+        */
+        public int MaxProfit(int[] prices) {
+            if (prices == null || prices.Length == 0) return 0;
+            int res = 0, min = prices[0];
+            
+            for (int i = 1; i < prices.Length; i++){
+                res = Math.Max(res, prices[i] - min);
+                min = Math.Min(min, prices[i]);
+            }
+            
+            return res;
+        }
+    }
+
+}
---
+++ Assets/Third/Medium/123.BestTimetoBuyandSellStockIII.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Third
+{
+
+    public class BestTimetoBuyandSellStockIII : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        /**
+        https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+        Hard
+
+
+        思路：Discuss中讨论提出了可以轻易得出递推公式, k次交易，在i次卖出
+        dp[k, i] = Math.Max(dp[k, i - 1], prices[i] - prices[j] + dp[k - 1, j - 1]);  for j from 0 to i - 1
+        
+        会导致TLE错误
+        */
+        public int MaxProfit(int[] prices) {
+            if (prices == null || prices.Length == 0) return 0;
+            int[,] dp = new int[3, prices.Length];
+            
+            for (int k = 1; k <= 2; k++){
+                for (int i = 1; i < prices.Length; i++){
+                    int min = prices[0];
+                    for (int j = 1; j <= i; j++){
+                        min = Math.Min(min, prices[j] - dp[k - 1, j - 1]);
+                    }
+                    dp[k, i] = Math.Max(dp[k, i - 1], prices[i] - min);
+                }
+            }
+            
+            return dp[2, prices.Length - 1];
+        }
+
+        /**
+        O(kn) 时间解法
+
+
+        */
+        public int MaxProfit_(int[] prices) {
+            if (prices == null || prices.Length == 0) return 0;
+            int[,] dp = new int[3, prices.Length];
+            
+            for (int k = 1; k <= 2; k++){
+                int min = prices[0];
+                for (int i = 1; i < prices.Length; i++){
+                    min = Math.Min(min, prices[i] - dp[k - 1, i - 1]);
+                    dp[k, i] = Math.Max(dp[k, i - 1], prices[i] - min);
+                }
+            }
+            
+            return dp[2, prices.Length - 1];
+        }
+
+    }
+
+}

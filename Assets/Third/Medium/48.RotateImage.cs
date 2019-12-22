f11f0e10ecfd397101bc4fb52aaccd110c097ee5
Dftty <tyzhangyj@163.com>
2019-12-22 14:35:57 +0800

    test

--- Assets/Third/Medium/48.RotateImage.cs
+++ Assets/Third/Medium/48.RotateImage.cs
@@ -47,6 +47,21 @@ namespace Third
                 }
             }
         }
+
+        /**
+        c++ 实现
+        void rotate(vector<vector<int>>& matrix) {
+            reverse(matrix.begin(), matrix.end());
+            
+            for (int i = 0; i < matrix.size(); i++){
+                for (int j = 0; j < i; j++){
+                    swap(matrix[i][j], matrix[j][i]);
+                }
+            }
+        }
+
+
+        */
     }
 
 }

5ae65e041a98bbe04f0a115de14b36ec3f85d05c
zyj <602954313@qq.com>
2020-01-03 07:21:55 +0800

    test

--- Assets/Third/Medium/1298.MaximumCandiesYouCanGetfromBoxes.cs
+++ Assets/Third/Medium/1298.MaximumCandiesYouCanGetfromBoxes.cs
@@ -31,47 +31,47 @@ namespace Third
         */
         public int MaxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
             HashSet<int> remainBox = new HashSet<int>();
-        HashSet<int> hasKeys = new HashSet<int>();
-        HashSet<int> hasBox = new HashSet<int>();
+            HashSet<int> hasKeys = new HashSet<int>();
+            HashSet<int> hasBox = new HashSet<int>();
 
-        for (int i = 0; i < status.Length; i++){
-            remainBox.Add(i);
-        }
-
-        Queue<int> q = new Queue<int>();
-        for (int i = 0; i < initialBoxes.Length; i++){
-            q.Enqueue(initialBoxes[i]);
-        }
-
-        int sum = 0;
-        while (q.Count > 0){
-            int index = q.Dequeue();
-            if (!remainBox.Contains(index)) continue;
-            if (status[index] == 0 && !hasKeys.Contains(index)){
-                hasBox.Add(index);
-                continue;
+            for (int i = 0; i < status.Length; i++){
+                remainBox.Add(i);
             }
 
-            remainBox.Remove(index);
-            hasKeys.Remove(index);
+            Queue<int> q = new Queue<int>();
+            for (int i = 0; i < initialBoxes.Length; i++){
+                q.Enqueue(initialBoxes[i]);
+            }
 
-            sum += candies[index];
+            int sum = 0;
+            while (q.Count > 0){
+                int index = q.Dequeue();
+                if (!remainBox.Contains(index)) continue;
+                if (status[index] == 0 && !hasKeys.Contains(index)){
+                    hasBox.Add(index);
+                    continue;
+                }
 
-            for (int i = 0; i < keys[index].Length; i++){
-                hasKeys.Add(keys[index][i]);
-                if (hasBox.Contains(keys[index][i]) && remainBox.Contains(keys[index][i])){
-                    q.Enqueue(keys[index][i]);
+                remainBox.Remove(index);
+                hasKeys.Remove(index);
+
+                sum += candies[index];
+
+                for (int i = 0; i < keys[index].Length; i++){
+                    hasKeys.Add(keys[index][i]);
+                    if (hasBox.Contains(keys[index][i]) && remainBox.Contains(keys[index][i])){
+                        q.Enqueue(keys[index][i]);
+                    }
+                }
+
+                for (int i = 0; i < containedBoxes[index].Length; i++){
+                    if (remainBox.Contains(containedBoxes[index][i])){
+                        q.Enqueue(containedBoxes[index][i]);
+                    }
                 }
             }
 
-            for (int i = 0; i < containedBoxes[index].Length; i++){
-                if (remainBox.Contains(containedBoxes[index][i])){
-                    q.Enqueue(containedBoxes[index][i]);
-                }
-            }
-        }
-
-        return sum;
+            return sum;
         }
     }
 
